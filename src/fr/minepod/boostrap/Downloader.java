961dec41bd91cfb4fd7f82cdc9af44b2cf62f321
DarkShimy00 <darkshimy00@gmail.com>
2013-07-15 18:43:17 +0100

    Auto-download Minecraft's libraries

--- src/fr/minepod/boostrap/Downloader.java
+++ src/fr/minepod/boostrap/Downloader.java
@@ -9,6 +9,11 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+
 public class Downloader {
 	 private FileOutputStream fos;
 	 private InputStream rbc;
@@ -24,9 +29,11 @@ public class Downloader {
    	 private URLConnection urlConnection;
 	 private String AppDataPath;
 	 private String fileType;
-	 private String latestVersion;
+	 private String latestVersionLauncher;
 	 private String MinePod;
-	private File MinePodLaunch;
+	 private File MinePodLaunch;
+	private String latestVersionLibraries;
+	private String MinePodLibrariesZip;
 	 
 	 private void Downloader(URL website, FileOutputStream fos) {
 		    System.out.println("Starting...");
@@ -64,6 +71,27 @@ public class Downloader {
 		    System.out.println("Downloading complete!");
 		  }
 	 
+	 public void UnZip(String fileInput, String folderOutput) {
+		   try {
+		         ZipFile zipFile = new ZipFile(fileInput);
+		         zipFile.extractAll(folderOutput);
+		    } catch (ZipException e) {
+		        e.printStackTrace();
+		    }
+	 }
+	 
+	 public void Zip(String folderInput, String fileOutput) {
+			try {
+				ZipFile zipFile = new ZipFile(fileOutput);
+				ZipParameters parameters = new ZipParameters();
+				parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+				parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+				zipFile.createZipFileFromFolder(folderInput, parameters, true, 10485760);
+			} catch (ZipException e) {
+				e.printStackTrace();
+			}
+	 }
+	 
 	 public void Launch() {
 		 try {
 			String OS = System.getProperty("os.name").toUpperCase();
@@ -81,21 +109,45 @@ public class Downloader {
 			
 			MinePod = "\\.MinePod";
 			MinePodLaunch = new File(AppDataPath + MinePod + "\\Launcher.jar");
+			MinePodLibrariesZip = AppDataPath + MinePod + "\\Libraries.zip";
 			
 			if(!new File(AppDataPath + MinePod).exists()) {
 				new File(AppDataPath + MinePod).mkdir();
 			}
 			
+			if(!new File(AppDataPath + MinePod + "\\Libraries").exists()) {
+				new File(AppDataPath + MinePod + "\\Libraries").mkdir();
+			}
 			
-			Downloader(new URL("http://assets.minepod.fr/launcher/versions/launcher.txt"), new FileOutputStream(AppDataPath + MinePod + "\\Launcher.txt"));
-			latestVersion = ReadFile.ReadFile(AppDataPath + MinePod + "\\Launcher.txt", StandardCharsets.UTF_8);
+			new File(AppDataPath + MinePod + "\\Launcher.txt").delete();
+			new File(AppDataPath + MinePod + "\\Launcher.md5").delete();
+			new File(AppDataPath + MinePod + "\\Libraries.txt").delete();
+			new File(AppDataPath + MinePod + "\\Libraries.zip").delete();
+			new File(AppDataPath + MinePod + "\\Libraries.md5").delete();
 			
-			Downloader(new URL("http://assets.minepod.fr/launcher/md5.php?file=" + latestVersion + ".jar"), new FileOutputStream(AppDataPath + MinePod + "\\Launcher.md5"));
+			
+			Downloader(new URL("http://assets.minepod.fr/launcher/versions/launcher.txt"), new FileOutputStream(AppDataPath + MinePod + "\\launcher.txt"));
+			latestVersionLauncher = ReadFile.ReadFile(AppDataPath + MinePod + "\\Launcher.txt", StandardCharsets.UTF_8);
+			
+			Downloader(new URL("http://assets.minepod.fr/launcher/md5.php?file=" + latestVersionLauncher), new FileOutputStream(AppDataPath + MinePod + "\\Launcher.md5"));
 			
 			if(!GetMd5.VerifyMd5(new File(AppDataPath + MinePod + "\\Launcher.md5"), MinePodLaunch)) {
 				MinePodLaunch.delete();
-				Downloader(new URL(latestVersion + ".jar"), new FileOutputStream(MinePodLaunch));
+				Downloader(new URL(latestVersionLauncher), new FileOutputStream(MinePodLaunch));
 			}
+			
+			Downloader(new URL("http://assets.minepod.fr/launcher/versions/libraries.txt"), new FileOutputStream(AppDataPath + MinePod + "\\Libraries.txt"));
+			latestVersionLibraries = ReadFile.ReadFile(AppDataPath + MinePod + "\\Libraries.txt", StandardCharsets.UTF_8);
+
+			Downloader(new URL("http://assets.minepod.fr/launcher/md5.php?file=" + latestVersionLibraries), new FileOutputStream(AppDataPath + MinePod + "\\Libraries.md5"));
+			Zip(AppDataPath + MinePod + "\\Libraries", MinePodLibrariesZip);
+
+			if(!GetMd5.VerifyMd5(new File(AppDataPath + MinePod + "\\Libraries.md5"), new File(MinePodLibrariesZip))) {
+				new File(MinePodLibrariesZip).delete();
+				Downloader(new URL(latestVersionLibraries), new FileOutputStream(MinePodLibrariesZip));
+				UnZip(MinePodLibrariesZip, AppDataPath + MinePod + "\\Libraries");
+				new File(MinePodLibrariesZip).delete();
+			}	
 			
 			new MPLoader().addURL(MinePodLaunch.toURI().toURL());
 
