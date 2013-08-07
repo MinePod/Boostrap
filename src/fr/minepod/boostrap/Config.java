d794115b83253fe94af4e18750264178504dff2c
DarkShimy00 <darkshimy00@gmail.com>
2013-08-07 13:54:27 +0100

    Updated debugging -> version 1.0.6

--- src/fr/minepod/boostrap/Config.java
+++ src/fr/minepod/boostrap/Config.java
@@ -6,5 +6,5 @@ public class Config {
 	 public String BootstrapNewVersionUrl = "http://assets.minepod.fr/launcher/bootstrap.php";
 	 public String GetMd5FileUrl = "http://assets.minepod.fr/launcher/md5.php?file=";
 	 public String LauncherName = "MinePod";
-	 public String BootstrapVersion = "1.0.5";
+	 public String BootstrapVersion = "1.0.6";
 }
--- src/fr/minepod/boostrap/JarLoader.java
+++ src/fr/minepod/boostrap/JarLoader.java
@@ -4,16 +4,18 @@ import java.awt.Desktop;
 import java.io.File;
 
 public class JarLoader {
+	private Config Config = new Config();
+	private String BootstrapVersion = Config.BootstrapVersion;
 	public JarLoader(String path) throws Exception {
 		String OS = System.getProperty("os.name").toUpperCase();
 		if(OS.contains("WIN")) {
-			Runtime.getRuntime().exec("java -jar -Xmx1G \"" + path + "\"");
+			Runtime.getRuntime().exec("java -jar -Xmx1G \"" + path + "\" " + BootstrapVersion);
 		} else if(OS.contains("MAC")) {
 			Desktop.getDesktop().open(new File(path));
 		} else if(OS.contains("NUX")) {
-			Runtime.getRuntime().exec("java -jar -Xmx1G \"" + path + "\"");
+			Runtime.getRuntime().exec("java -jar -Xmx1G \"" + path + "\" " + BootstrapVersion);
 		} else {
-			Runtime.getRuntime().exec("java -jar -Xmx1G \"" + path + "\"");
+			Runtime.getRuntime().exec("java -jar -Xmx1G \"" + path + "\" " + BootstrapVersion);
 		}
 	}
 }
